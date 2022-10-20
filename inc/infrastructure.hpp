49440e237f0134f29c337a6bd747e30e9feeb042
Andreas Englert <aenglert@3-C-8.42heilbronn.de>
2022-10-20 18:01:38 +0200

    updated get tests for webserv

--- inc/infrastructure.hpp
+++ inc/infrastructure.hpp
@@ -10,6 +10,8 @@
 // directories
 #define ROOT "./server/root/"
 #define DIR ROOT"dir/"
+#define ONE DIR"one/"
+#define TWO ONE"two/"
 #define PERMISSION_DIR DIR"permission_dir/"
 #define NO_PERMISSION_DIR DIR"no_permission_dir/"
 #define UPLOADS DIR"uploads/"
@@ -17,6 +19,7 @@
 #define CUSTOM_INDEX INDEX"custom/"
 #define AUTOINDEX INDEX"no/autoindex/"
 #define NO_AUTOINDEX INDEX"no/no_autoindex/"
+#define AUTOINDEX_NO_PERMISSION_DIR AUTOINDEX"no_permission_dir/"
 
 #define SERVER1 ROOT"server1/"
 #define SERVER1_CUSTOM SERVER1"custom/"
@@ -26,6 +29,7 @@
 // files
 #define FILE "file"
 #define NO_PERMISSION_FILE "no_permission_file"
+#define NO_PERMISSION_FILE_CGI "no_permission_file.cgi"
 #define DOT_CGI ".cgi"
 #define CGI_FILE "file.cgi"
 #define INDEX_HTML "index.html"
--- src/infrastructure.cpp
+++ src/infrastructure.cpp
@@ -13,7 +13,9 @@ static void set_permission(const std::string& directory, const std::string& file
 static void apply_permissions(void)
 {
 	set_permission(PERMISSION_DIR, NO_PERMISSION_FILE, std::filesystem::perms::none);
+	set_permission(UPLOADS, NO_PERMISSION_FILE_CGI, std::filesystem::perms::none);
 	set_permission(NO_PERMISSION_DIR, std::filesystem::perms::owner_read | std::filesystem::perms::owner_write);
+	set_permission(AUTOINDEX_NO_PERMISSION_DIR, std::filesystem::perms::owner_write);
 }
 
 static void create_file_with_content(const std::string& directory, const std::string& file, const std::string& content)
@@ -43,6 +45,13 @@ static void create_files(void)
 	create_file_with_content(DIR, "%file");
 	create_file_with_content(ROOT, INDEX_HTML);
 
+	create_file_with_content(ONE, CUSTOM_HTML);
+
+	create_file_with_content(TWO, INDEX_HTML);
+
+	create_file_with_content(INDEX, INDEX_HTML);
+
+	create_file_with_content(CUSTOM_INDEX, CUSTOM_HTML);
 	
 	create_file_with_content(PERMISSION_DIR, FILE);
 	create_file_with_content(PERMISSION_DIR, NO_PERMISSION_FILE);
@@ -53,20 +62,23 @@ static void create_files(void)
 	create_file_with_content(NO_PERMISSION_DIR, CGI_FILE);
 
 	create_file_with_content(UPLOADS, NO_PERMISSION_FILE);
-
-	create_file_with_content(CUSTOM_INDEX, CUSTOM_HTML);
+	create_file_with_content(UPLOADS, NO_PERMISSION_FILE_CGI);
 
 	create_file_with_content(SERVER1_CUSTOM, CUSTOM_HTML, "MY_CUSTOM_PAGE");
 }
 
 static void create_directories(void)
 {
+	std::filesystem::create_directories(TWO);
 	std::filesystem::create_directories(PERMISSION_DIR);
 	std::filesystem::create_directories(NO_PERMISSION_DIR);
 	std::filesystem::create_directories(UPLOADS);
 	std::filesystem::create_directories(CUSTOM_INDEX);
 	std::filesystem::create_directories(AUTOINDEX);
 	std::filesystem::create_directories(NO_AUTOINDEX);
+	std::filesystem::create_directories(AUTOINDEX_NO_PERMISSION_DIR);
+	std::string tmp = AUTOINDEX;
+	std::filesystem::create_directories(tmp + "autoindex123/");
 
 	std::filesystem::create_directories(SERVER1_CUSTOM);
 	std::filesystem::create_directories(SERVER2);
@@ -76,6 +88,8 @@ void clear_infrastructure(void)
 {
 	if (std::filesystem::exists(NO_PERMISSION_DIR))
 		set_permission(NO_PERMISSION_DIR, std::filesystem::perms::owner_all);
+	if (std::filesystem::exists(AUTOINDEX_NO_PERMISSION_DIR))
+		set_permission(AUTOINDEX_NO_PERMISSION_DIR, std::filesystem::perms::owner_all);
 	std::filesystem::remove_all(ROOT);
 }
 
